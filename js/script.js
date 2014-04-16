b2072e967c88313cb7fbaf99cf535a19cdf73b0d
puhrome <phirom.yim@fullsail.edu>
2014-04-16 19:08:57 -0400

    Celsius to Fahrenheit Converter
    Using conditional expression and Intro to JavaScript textbook to write
    equation to convert cent to fahren with WebStorm

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Celsius to Fahrenheit converter Created by Phirom Yim on 4/14/14.
+ */
+//first data sets
+var degFahren = Number(prompt("Enter the degrees Fahrenheit",32));
+var degCent;
+degCent = 5/9 * (degFahren - 32);
+//if celsius is 0 then fahrenheit is 32
+console.log(degFahren + "\xB0 The temperature is " + degCent + " \xB0 Celsius")
+if(degCent < 0) {
+    console.log("\xB0 The temperature is " + degFahren + " \xB0 Fahrenheit");
+
+}
+
+
+
+
