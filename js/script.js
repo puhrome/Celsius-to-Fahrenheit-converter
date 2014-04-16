67158fbd5ba786a78ebf5426b5ae62387e3ec0dd
puhrome <phirom.yim@fullsail.edu>
2014-04-16 19:15:10 -0400

    added comments
    updated comments in comment line

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
  * Celsius to Fahrenheit converter Created by Phirom Yim on 4/14/14.
  */
 //first data sets
-var degFahren = Number(prompt("Enter the degrees Fahrenheit"));
-var degCent;
-degCent = 5/9 * (degFahren - 32);
-//if celsius is 0 then fahrenheit is 32
+var degFahren = Number(prompt("Enter the degrees Fahrenheit")); //prompt for user input
+var degCent; //declaring celsius variable
+degCent = 5/9 * (degFahren - 32); //defining formula for conversion
+//5 divided by 9 times the degree in Fahrenheit minus 32
 console.log(degFahren + "\xB0 The temperature is " + degCent + " \xB0 Celsius")
 if(degCent < 0) {
     console.log("\xB0 The temperature is " + degFahren + " \xB0 Fahrenheit");
